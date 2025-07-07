ed30b8cdec8942755bddbbec27897ddb6c084d71
Tabish Raza <tabishraza586@gmail.com>
2025-07-08 02:40:47 +0530

    new update

--- backend/backend/config/mongodb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database connected"));
-  mongoose.connect(`${process.env.MONGODB_URL}/prescripto`);
-};
-
-export default connectDB;
--- backend/backend/routes/doctorRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { doctorList } from '../controller/doctorController.js'
-
-const doctorRouter = express.Router()
-
-doctorRouter.get("/list", doctorList)
-
-export default doctorRouter
